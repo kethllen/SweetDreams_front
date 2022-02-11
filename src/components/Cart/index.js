f577c9b204ac4345359670bf918851dbe5c3cce4
kethllen <kety_andrey@hotmail.com>
2022-02-11 15:23:13 -0300

    cart xabu

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,7 +1,32 @@
-import { HeaderDiv, HeaderContainer } from "./styled";
-import cart from "../../assets/cart.png";
-import { TiArrowBack } from "react-icons/ti";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
 import Header from "../Header";
+import UserContext from "../../contexts/UserContext";
+
 export default function Cart() {
+  const { token, name } = useContext(UserContext);
+  const { cartItems, setCartItems } = useState([]);
+  const navigate = useNavigate();
+  if (!token) {
+    navigate("/cadastro");
+  }
+  console.log(token);
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    console.log(config);
+    const promise = axios.get(process.env.REACT_APP_BACK_URL + "cart", config);
+    promise.then((response) => {
+      setCartItems(response.data);
+      console.log(cartItems);
+    });
+    promise.catch((error) => {
+      alert(error.response.data.message);
+    });
+  }, []);
   return <Header main={false} />;
 }
