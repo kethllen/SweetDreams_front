b4d92afb683c8bd35b68a0991748531fd9638d8c
kethllen <kety_andrey@hotmail.com>
2022-02-11 12:57:02 -0300

    layout header

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,16 +1,7 @@
 import { HeaderDiv, HeaderContainer } from "./styled";
 import cart from "../../assets/cart.png";
 import { TiArrowBack } from "react-icons/ti";
+import Header from "../Header";
 export default function Cart() {
-  return (
-    <HeaderContainer>
-      <HeaderDiv>
-        <TiArrowBack color="#000000" size={25} />
-        <h1>SweetDreams</h1>
-        <div className="cart">
-          <img src={cart} alt="" />
-        </div>
-      </HeaderDiv>
-    </HeaderContainer>
-  );
+  return <Header main={false} />;
 }
