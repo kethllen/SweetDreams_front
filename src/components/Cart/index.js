5a57e366386ebdd035a61b3b60135c2bf76ad55e
kethllen <kety_andrey@hotmail.com>
2022-02-11 22:56:54 -0300

    layout cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -3,30 +3,73 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 import Header from "../Header";
 import UserContext from "../../contexts/UserContext";
+import {
+  HeaderContainer,
+  Title,
+  HeaderDiv,
+  CartDiv,
+  CartContainer,
+  CartNome,
+  Total,
+  Button,
+} from "./styled";
 
 export default function Cart() {
   const { token, name } = useContext(UserContext);
-  const { cartItems, setCartItems } = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  let total = 0;
   const navigate = useNavigate();
   if (!token) {
     navigate("/cadastro");
   }
   console.log(token);
   useEffect(() => {
-    const config = {
+    const promise = axios.get(process.env.REACT_APP_BACK_URL + "cart", {
       headers: {
-        Authorization: `Bearer ${token}`,
+        authorization: `Bearer ${token}`,
       },
-    };
-    console.log(config);
-    const promise = axios.get(process.env.REACT_APP_BACK_URL + "cart", config);
+    });
     promise.then((response) => {
+      console.log(response.data);
       setCartItems(response.data);
-      console.log(cartItems);
     });
     promise.catch((error) => {
       alert(error.response.data.message);
     });
   }, []);
-  return <Header main={false} />;
+  cartItems.map((product) => {
+    const aux = product.subtotal.replace(",", ".");
+    total += parseFloat(aux);
+  });
+  return (
+    <>
+      <Header main={false} />
+      <Title>Cart</Title>
+      <HeaderContainer>
+        <HeaderDiv>
+          <h1>CART ITEMS</h1>
+          <h2>PRICE</h2>
+          <h2>QUANTITY</h2>
+          <h2>SUBTOTAL</h2>
+        </HeaderDiv>
+      </HeaderContainer>
+      <CartContainer>
+        {cartItems.map((product) => {
+          return (
+            <CartDiv>
+              <CartNome>
+                <img src={product.image} />
+                <h1>{product.name}</h1>
+              </CartNome>
+              <h2>R$ {product.price}</h2>
+              <h2>{product.quantity}</h2>
+              <h2>R$ {product.subtotal}</h2>
+            </CartDiv>
+          );
+        })}
+        <Total>Total: R$ {total}</Total>
+        <Button>Checkout</Button>
+      </CartContainer>
+    </>
+  );
 }
--- src/components/MainPage/style.js
+++ src/components/MainPage/style.js
@@ -1,77 +1,77 @@
 import styled from "styled-components";
 
 const ProductsContainer = styled.div`
-    width: 100%;
-    
-    padding-top: 80px;
-    padding-bottom: 30px;
+  width: 100%;
 
-    display: flex; 
-    justify-content: center;
-`
+  padding-top: 80px;
+  padding-bottom: 30px;
+
+  display: flex;
+  justify-content: center;
+`;
 
 const CentralizedDiv = styled.div`
-    width: 900px;
-    
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-column-gap: 12px;
-    grid-row-gap: 40px;
+  width: 900px;
 
-    justify-content: center;
-`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 12px;
+  grid-row-gap: 40px;
+
+  justify-content: center;
+`;
 
 const Product = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    text-align: center;
+  text-align: center;
 
-    img {
-        width: 100%;
-        height: 210px;
+  img {
+    width: 100%;
+    height: 210px;
 
-        object-fit: cover;
+    object-fit: cover;
 
-        margin-bottom: 12px;
-    }
+    margin-bottom: 12px;
+  }
 
-    .name {
-        font-size: 14px;
+  .name {
+    font-size: 14px;
 
-        margin-bottom: 12px;
-    }
+    margin-bottom: 12px;
+  }
 
-    .description {
-        font-size: 8px;
+  .description {
+    font-size: 8px;
 
-        margin-bottom: 20px;
-    }
+    margin-bottom: 20px;
+  }
 
-    .price {
-        font-size: 12px;
+  .price {
+    font-size: 12px;
 
-        margin-bottom: 8px;
-    }
+    margin-bottom: 8px;
+  }
 
-    button {
-        width: 100%;
+  button {
+    width: 100%;
 
-        font-size: 13px;
-        font-weight: 700;
-        text-transform: uppercase;
-        color: #00ced1;
+    font-size: 13px;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #00ced1;
 
-        padding: 10px 0;
-        
-        background-color: #FFFFFF;
+    padding: 10px 0;
 
-        border-radius: 10px;
-        border: 2px solid #00ced1;
+    background-color: #ffffff;
 
-        cursor: pointer;
-    }
-`
+    border-radius: 10px;
+    border: 2px solid #00ced1;
 
-export { ProductsContainer, CentralizedDiv, Product }
+    cursor: pointer;
+  }
+`;
+
+export { ProductsContainer, CentralizedDiv, Product };
